398c966a6dfd531d723af9f8e0a81728dbc223f4
s <as>
2023-11-30 00:01:43 -0400

    listo area de banner-hero

---
+++ src/assets/img-hero/index.js
@@ -0,0 +1,8 @@
+import jaguarImg from './Jaguar-hero.jpeg'
+import osoAnteojos from './ojo-anteojos.webp'
+import ballenasFrancas from './ballenas-francas.webp'
+
+export {
+  jaguarImg,osoAnteojos,ballenasFrancas
+
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import { Outlet, useOutlet } from 'react-router-dom'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import Landing from '../../components/Landing'
+import Hero from '../../components/Hero'
 
 export default function Home() {
   const outlet = useOutlet()
@@ -9,6 +10,7 @@ export default function Home() {
     <>
       <Header />
       <hr />
+      <Hero/>
       <main>{outlet ? <Outlet /> : <Landing />}</main>
       <hr />
       <Footer />
