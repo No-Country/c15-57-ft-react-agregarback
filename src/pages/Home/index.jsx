3425f5b43ec2e1852b5af83264484fd14ad10c16
fabio <fabio@digitalmind.com>
2023-11-30 17:06:41 -0300

    Resolución de conflictos

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,6 @@ import { Outlet, useOutlet } from 'react-router-dom'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import Landing from '../../components/Landing'
-import Hero from '../../components/Hero'
 
 export default function Home() {
   const outlet = useOutlet()
@@ -10,7 +9,6 @@ export default function Home() {
     <>
       <Header />
       <hr />
-      <Hero/>
       <main>{outlet ? <Outlet /> : <Landing />}</main>
       <hr />
       <Footer />
