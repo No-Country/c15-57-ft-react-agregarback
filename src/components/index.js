561c3d5d4edadafc7cf43c8b2da281e5a849c2af
fabio <fabio@digitalmind.com>
2023-12-01 13:39:19 -0300

    [FIX] errores de importación arregla2

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
-import Landing from './Landing/Landing';
-import Footer from "./Footer/Footer";
-import Header from './Header/Header';
-import Hero from '../components/Hero/index.jsx';
+import Landing from './Landing/Landing'
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
+import Hero from '../components/Hero'
+import Button from '../components/Button'
+import OtherSesion from '../components/OtherSesion'
 
-export { Landing, Footer, Header, Hero };
+export { Landing, Footer, Header, Hero, Button, OtherSesion }
