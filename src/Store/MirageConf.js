1a5cfb35dce18dd539394134002e7e84d9e4704b
Natsumychan <dianaczapata@gmail.com>
2023-12-06 14:29:35 -0500

    added  fake session on google and facebook

--- src/Store/MirageConf.js
+++ src/Store/MirageConf.js
@@ -152,6 +152,28 @@ export function makeServer () {
           return { message: 'Usuario no encontrado' }
         }
       })
+      this.post('/google-login', (_, request) => {
+        // Aquí puedes simular el inicio de sesión con Google
+        // Devuelve los datos del usuario simulados para este ejemplo
+        return {
+          user: {
+            id: crypto.randomUUID(),
+            name: 'Usuario de Google',
+            email: 'google@example.com'
+          }
+        }
+      })
+      this.post('/facebook-login', (_, request) => {
+        // Aquí puedes simular el inicio de sesión con Facebook
+        // Devuelve los datos del usuario simulados para este ejemplo
+        return {
+          user: {
+            id: crypto.randomUUID(),
+            name: 'Usuario de Facebook',
+            email: 'facebook@example.com'
+          }
+        }
+      })
       this.delete('/:id', (schema, request) => {
         const id = request.params.id
         return schema.users.find(id).destroy()
--- src/components/OtherSesion/index.jsx
+++ src/components/OtherSesion/index.jsx
@@ -1,17 +1,34 @@
 import { FcGoogle } from 'react-icons/fc'
 import { BsFacebook } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default function OtherSesion ({ to, text }) {
+  const handleGoogleLogin = async () => {
+    try {
+      const response = await axios.post('/api/users/google-login')
+      console.log('Usuario de Google:', response.data.user)
+    } catch (error) {
+      console.error('Error al iniciar sesión con Google:', error.response.data.error)
+    }
+  }
+  const handleFacebookLogin = async () => {
+    try {
+      const response = await axios.post('/api/users/facebook-login')
+      console.log('Usuario de Facebook:', response.data.user)
+    } catch (error) {
+      console.error('Error al iniciar sesión con Facebook:', error.response.data.error)
+    }
+  }
   return (
     <>
       <div className='flex flex-col gap-2 pt-2 mt-3'>
-        <button className='flex justify-center rounded bg-neutral-100 item-center border '>
+        <button className='flex justify-center rounded bg-neutral-100 item-center border' onClick={handleGoogleLogin}>
           <FcGoogle />
           <span className='py-1.5 text-center w-9/12'>Iniciar con Google</span>
         </button>
 
-        <button className='flex justify-center rounded bg-neutral-100 item-center border '>
+        <button className='flex justify-center rounded bg-neutral-100 item-center border ' onClick={handleFacebookLogin}>
           {' '}
           <BsFacebook className='fle content-center' />
           <span className='py-1.5 text-center w-9/12'>
