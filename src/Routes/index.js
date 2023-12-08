0cde467eda1ab5c2b45f3be5640287dff4d2c6d8
JRIVERADDIAZ <67973899+JRIVERADDIAZ@users.noreply.github.com>
2023-12-08 05:41:30 +0000

    logo-nav.added

--- src/Routes/index.js
+++
@@ -1,8 +0,0 @@
-import Contact from './Contact/Component/Contact'
-import Home from './Home/Component/Home'
-import Login from './Login/Presentation/Login'
-import NotFound from './NotFound/Component/NotFound'
-import Register from './Register/Presentation/Register'
-import Store from './Store/Component/Store'
-
-export { Contact, Home, Login, NotFound, Register, Store }
