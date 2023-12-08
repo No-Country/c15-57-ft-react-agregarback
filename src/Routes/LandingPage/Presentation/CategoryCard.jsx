0b24f2687892867a9e5f6b7e7c7bab8692aa43f6
s <as>
2023-12-08 11:04:37 -0500

    add all info to landing

--- src/Routes/LandingPage/Presentation/CategoryCard.jsx
+++ src/Routes/LandingPage/Presentation/CategoryCard.jsx
@@ -2,7 +2,7 @@ const CategoryCard = ({animal}) =>{
 
   return (
     <figure className='mt-[26px] w-[263px] h-[370px]'>
-    <img className='w-full h-full flex-shrink-0 bg-cover bg-no-repeat category-img ' src={animal?.img} alt={animal?.name} />
+    <img className='w-full h-full flex-shrink-0 bg-cover bg-no-repeat rounded-[12px] ' src={animal?.img} alt={animal?.name} />
     </figure>
   
 )}
