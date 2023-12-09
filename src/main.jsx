5817c48bc24171eb7a500d802eabedf9fa2f9f59
JRIVERADDIAZ <67973899+JRIVERADDIAZ@users.noreply.github.com>
2023-12-09 21:16:47 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './Router'
-import { makeServer } from './Store/Server/MirageConf'
-
 import ContentProvider from './Store/contextStore/ContentContext'
 import UserProvider from './Store/contextStore/UserContext'
 import ECommerceProvider from './Store/contextStore/ECommerceContext'
-
-
-
-makeServer()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ContentProvider>
