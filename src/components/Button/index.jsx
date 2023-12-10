95c8876e889bb593ff47a934e38d523ab28053cf
Natsumychan <dianaczapata@gmail.com>
2023-12-10 01:26:12 -0500

    fixed width responsive on Button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,7 +4,7 @@ export default function Button (props) {
       <div className='flex items-center justify-center mb-3'>
         <input
           type='submit'
-          className={`${props.color} ${props.hover} font-normal w-[65%] mt-5 text-white py-2 px-4 rounded-full focus:outline-none focus:shadow-outline`}
+          className={`${props.color} ${props.hover} font-normal mt-5 text-white py-2 px-4 rounded-full focus:outline-none focus:shadow-outline sm:w-[80%] md:w-[65%]`}
           value={props.text}
         />
       </div>
