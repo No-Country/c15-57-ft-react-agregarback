b1dcde7e42799e40184b1556019f0acf89dc346b
Natsumychan <dianaczapata@gmail.com>
2023-12-10 01:24:43 -0500

    added InputForm and InputSelectForm component

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,15 @@
-import Footer from './Footer/Component/Footer';
+import Footer from './Footer/Component/Footer'
 import Header from './Header/Header'
 import Button from './Button/'
-import OtherSesion from '../components/OtherSesion';
-import NavBar from './NavBar/Component/NavBar';
-
+import OtherSesion from '../components/OtherSesion'
+import NavBar from './NavBar/Component/NavBar'
+import InputForm from './InputForm/Component/index'
 import Input from '../components/Input/Component'
-import InputPassword from './InputPassword';
-import CardStoreContainerCard from './StoreCard/Component/CardStore';
-import LandingPage from '../components/Carrousel/Component/LandingPage';
+import InputPassword from './InputPassword'
+import InputSelectForm from './InputSelectForm/Component'
+import CardStoreContainerCard from './StoreCard/Component/CardStore'
+import LandingPage from '../components/Carrousel/Component/LandingPage'
 
-import InfoPageContainer, { loaderItem } from './InfoPage/Container/InfoPage';
+import InfoPageContainer, { loaderItem } from './InfoPage/Container/InfoPage'
 
-export { LandingPage, Footer, Header, OtherSesion, Input, InputPassword, Button, CardStoreContainerCard, InfoPageContainer, loaderItem, NavBar }
+export { LandingPage, Footer, Header, OtherSesion, Input, InputPassword, Button, CardStoreContainerCard, InfoPageContainer, loaderItem, NavBar, InputForm, InputSelectForm }
