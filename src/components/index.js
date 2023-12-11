2e64010d91c8854379561c7309eea0a79fd903d3
JRIVERADDIAZ <67973899+JRIVERADDIAZ@users.noreply.github.com>
2023-12-11 10:23:44 +0000

    files-not-workig-deleted

--- src/components/index.js
+++ src/components/index.js
@@ -5,9 +5,9 @@ import NavBar from './MainNavBar/NavBar/Component/NavBar';
 
 import Input from '../components/Input/Component'
 import InputPassword from './InputPassword';
-import CardStoreContainerCard from './StoreCard/Component/CardStore';
+
 import LandingPage from '../components/Carrousel/Component/LandingPage';
 
-import InfoPageContainer, { loaderItem } from './InfoPage/Container/InfoPage';
 
-export { LandingPage, Footer, OtherSesion, Input, InputPassword, Button, CardStoreContainerCard, InfoPageContainer, loaderItem, NavBar }
+
+export { LandingPage, Footer, OtherSesion, Input, InputPassword, Button, NavBar }
