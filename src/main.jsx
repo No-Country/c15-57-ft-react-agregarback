01a7218d211ad9274aba0f5bc74ec7f43495f042
JRIVERADDIAZ <jovahinrivera@gmail.com>
2023-12-12 21:07:52 +0000

    version02-layout-completado

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import {router} from './Router'
 import ContentProvider from './Store/contextStore/ContentContext'
 import UserProvider from './Store/contextStore/UserContext'
 import ECommerceProvider from './Store/contextStore/ECommerceContext'
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev/index.js";
 import { makeServer } from './Store/Server/MirageConf'
 
 makeServer()
@@ -15,11 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ContentProvider>
         <ECommerceProvider>
             <UserProvider>
-                <DevSupport ComponentPreviews={ComponentPreviews}
-                            useInitialHook={useInitial}
-                >
+              
                     <RouterProvider router={router}/>
-                </DevSupport>
+        
             </UserProvider>
         </ECommerceProvider>
     </ContentProvider>
