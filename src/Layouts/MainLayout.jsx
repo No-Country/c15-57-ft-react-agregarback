dc338f6ed9e5d1b31f779d5713ce55c7936b6218
fabio <fabio@digitalmind.com>
2023-12-13 18:26:58 -0300

    [DEL] hr del mainlayout

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { Footer, NavBar } from '../components';
+import { Outlet } from 'react-router-dom'
+import { Footer, NavBar } from '../components'
 
 import '../index.css'
 
 const MainLayout = () => {
-    return (
-        <div className="
+  return (
+    <div className='
     w-full
     h-full
     justify-self-center
@@ -14,20 +13,19 @@ const MainLayout = () => {
     grid-cols-1
     grid-rows-[1fr,auto,1fr]
     lg:grid-rows-[1r,auto,1fr]
-">
-            <header>
-                <NavBar/>
-            </header>
-            <hr/>
-            <main className='row-span-2 '>
-                <Outlet/>
-            </main>
-            <hr/>
-            <footer className='bg-amber-500'>
-                <Footer/>
-            </footer>
-        </div>
-    );
-};
+'
+    >
+      <header>
+        <NavBar />
+      </header>
+      <main className='row-span-2 '>
+        <Outlet />
+      </main>
+      <footer className='bg-amber-500'>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
 
 export default MainLayout
