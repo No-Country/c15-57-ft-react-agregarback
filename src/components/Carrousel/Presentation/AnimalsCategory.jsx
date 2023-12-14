cf3eebd14582fbe212999e96fc1bb0f09946029e
fabio <fabio@digitalmind.com>
2023-12-14 12:47:30 -0300

    [FIX] animalsCategory

--- src/components/Carrousel/Presentation/AnimalsCategory.jsx
+++ src/components/Carrousel/Presentation/AnimalsCategory.jsx
@@ -1,43 +1,55 @@
 import CategoryCard from './CategoryCard'
-import { categoryBG } from '../../../assets/img-hero'
-import { leftIcon, rigthIcon } from "../../../assets/img-hero";
+import { categoryBG, leftIcon, rigthIcon } from '../../../assets/img-hero'
+
 import './style.css'
 
 const AnimalsCategory = ({ animals }) => {
   return (
     <section className='relative min-w-[430px] sm:pl-20  sm:mb-40 sm:w-[1440px] lg:mx-auto h-[660px]  pt-[86px] pb-[48px]  mb-20 '>
-
-         <figure className="bg-category-Animals h-[630px] w-[430px] sm:w-screen sm:max-w-[1220px] sm:h-[670px]  absolute top-0  overflow-hidden ">
-          <img className='w-[100%] h-[100%] z-0  flex-shrink-0 xl:w-full xl:h-full sm:object-cover' src={categoryBG} alt='BackGround-category' />
-          </figure>
+      <figure className='bg-category-Animals h-[630px] w-[430px] sm:w-screen sm:max-w-[1220px] sm:h-[670px]  absolute top-0  overflow-hidden '>
+        <img
+          className='w-[100%] h-[100%] z-0  flex-shrink-0 xl:w-full xl:h-full sm:object-cover'
+          src={categoryBG}
+          alt='BackGround-category'
+        />
+      </figure>
       <div className=' sm:m-w-[1080px] h-[630px]  mx-auto relative  z-10'>
         <div className='w-full flex flex-col items-center justify-center'>
-          <p className='sm:font-semibold mt-5 sm:text-[35px] text-[24px] mb-3'>Productos por animales</p>
+          <p className='sm:font-semibold mt-5 sm:text-[35px] text-[24px] mb-3'>
+            Productos por animales
+          </p>
           <div className='flex '>
-            <p className='font-light  text-[18px] '>Mira todas las carateristicas </p>
+            <p className='font-light  text-[18px] '>
+              Mira todas las carateristicas{' '}
+            </p>
             <div className=' hover:cursor-pointer w-8 flex justify-center items-center hover:font-bold'>
-              <img className='h-4' src={rigthIcon} alt="" />
-
-
+              <img
+                className='h-4'
+                src={rigthIcon}
+                alt=''
+              />
             </div>
           </div>
         </div>
         <div className='w-full m-w-[1440%] h-full flex flex-col items-center px-[30px] pt-[40px]'>
           <div className='w-full h-full flex gap-[40px]  justify-center '>
             {animals.map((animal, index) => (
-              <CategoryCard key={index} animal={animal} />
+              <CategoryCard
+                key={index}
+                animal={animal}
+              />
             ))}
-      <div className='h-full w-10 flex justify-center items-center text-[32px] ' >
-        <img className='sm:hidden' src={rigthIcon} alt="" />
-      </div>
-    </section>
-
-
-
+          </div>
         </div>
+      </div>
+      <div className='h-full w-10 flex justify-center items-center text-[32px] '>
+        <img
+          className='sm:hidden'
+          src={rigthIcon}
+          alt=''
+        />
       </div>
     </section>
   )
 }
-
 export default AnimalsCategory
