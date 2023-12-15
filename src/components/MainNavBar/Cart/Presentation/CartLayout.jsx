a1a842c3d10c9c0cb1317a49b3324e045da68bae
Natsumychan <dianaczapata@gmail.com>
2023-12-15 14:18:59 -0500

    link cart icon with ShoppingCart page

--- src/components/MainNavBar/Cart/Presentation/CartLayout.jsx
+++ src/components/MainNavBar/Cart/Presentation/CartLayout.jsx
@@ -1,10 +1,12 @@
-
+import { Link } from 'react-router-dom'
 import CartCounter from '../../CartCounter/Component/CartCounter'
-export const CartLayout = ({children}) => {
-    return (
-        <figcaption className=" md:relative">
-            {children}
-            <CartCounter />
-        </figcaption>
-    )
+export const CartLayout = ({ children }) => {
+  return (
+    <figcaption className=' md:relative'>
+      <Link to='/ShoppingCart'>
+        {children}
+        <CartCounter />
+      </Link>
+    </figcaption>
+  )
 }
