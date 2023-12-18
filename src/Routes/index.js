79396ccee03df7049a410f4d2d07ab1b59620a81
fabio <fabio@digitalmind.com>
2023-12-18 18:34:23 -0300

    [fix] chequando si es el componente ecosystems

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -14,6 +14,9 @@ const ShippingInformation = lazy(() => import('./ShippingInformation/Component/S
 const ShoppingCart = lazy(() => import('./ShoppingCart/Component/ShoppingCart'))
 const Donations = lazy(() => import('./Donations/Component/Donations'))
 const Thanks = lazy(() => import('./Thanks/Component/Thanks'))
+const ShoppingSuccessful = lazy(() => import('./ShoppingSuccessful/Component/ShoppingSuccessful'))
+
+
 
 const EcosystemPage = lazy(() => import('../Routes/AnimalsEcosystem/Componets/EcosystemPage'))
 // Protected Routes
@@ -22,4 +25,5 @@ const User = lazy(() => import('./User/Container/User'))
 const MyVolunteers = lazy(() => import('./MyPurchases/Container/MyPurchases'))
 const MyPurchases = lazy(() => import('./MyPurchases/Container/MyPurchases'))
 
-export { Animals, WhoWeAre, VulnerableSpecies, Login, NotFound, Register, Store, User, MyVolunteers, MyPurchases, ResetPassword, ShippingInformation, ShoppingCart, Donations }
+export { Animals, WhoWeAre, VulnerableSpecies, Thanks, Login, NotFound, Register, Store, User, MyVolunteers, MyPurchases, ResetPassword, ShippingInformation, ShoppingCart, Donations, ShoppingSuccessful,EcosystemPage }
+
