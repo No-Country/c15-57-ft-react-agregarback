4a8fcacbafa2545ecbf53db2fd132caf760a5bf5
JRIVERADDIAZ <67973899+JRIVERADDIAZ@users.noreply.github.com>
2023-12-19 23:03:08 +0000

    Update EcosystemModal.jsx

--- src/Routes/EcosystemModal/Component/EcosystemModal.jsx
+++ src/Routes/EcosystemModal/Component/EcosystemModal.jsx
@@ -1,1 +1,19 @@
-src/Routes/EcosystemModal/Component/EcosystemModal.jsx
+import React from 'react'
+import { useLoaderData } from "react-router-dom";
+
+import EcosystemModalContainer from '../Presentation/EcosystemModal'
+
+function EcosystemModal() {
+  const { post } = useLoaderData();
+  return <EcosystemModalContainer post={post} />
+}
+
+export default EcosystemModal
+
+export const loaderPost = async ({ params }) => {
+  const data = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+  const post = await data.json();
+  return { post };
+};
