1ef41f36874add0a6db75544942c8256cba70dd5
fabio <fabio@digitalmind.com>
2023-12-19 10:41:37 -0300

    [ADD] Link to habitats

--- src/components/HabitatList/Presentation/HabitatList.jsx
+++ src/components/HabitatList/Presentation/HabitatList.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 export default function HabitatList ({ habitats }) {
   return (
     <section className='h-[800px] flex flex-col items-center bg-[white] pt-[90px]'>
@@ -6,7 +8,8 @@ export default function HabitatList ({ habitats }) {
       </h2>
       <div className='flex justify-center flex-row pb-[90px]'>
         {habitats.map((habitat) => (
-          <div
+          <Link
+            to='#'
             key={habitat.name}
             style={{ backgroundImage: `url(${habitat.img})` }}
             className='group w-[224.273px] h-[224.273px] shrink-0 cursor-pointer flex text-center justify-center items-center text-[color:var(--White,#FFF)] m-[18px]'
@@ -14,7 +17,7 @@ export default function HabitatList ({ habitats }) {
             <p className='opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center text-3xl not-italic font-bold leading-10'>
               {habitat.name}
             </p>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
