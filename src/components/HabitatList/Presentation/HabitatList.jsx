d2eca2562ee9a998be4a61565cf77e9bb7d577d0
fabio <fabio@digitalmind.com>
2023-12-19 19:03:19 -0300

    [fix] hover on ecosystems & habitats

--- src/components/HabitatList/Presentation/HabitatList.jsx
+++ src/components/HabitatList/Presentation/HabitatList.jsx
@@ -3,20 +3,22 @@ import { Link } from 'react-router-dom'
 export default function HabitatList ({ habitats }) {
   return (
     <section className=' flex flex-col items-center bg-[white] py-[90px]'>
-      <h2 className='text-[#1E1E1E] text-[32px] not-italic font-light leading-6 py-5 '>
+      <h2 className='text-[#1E1E1E] text-4xl not-italic font-light leading-6 py-10 '>
         Explorar los animales según su habitat
       </h2>
-      <div className='flex justify-center flex-row '>
+      <div className='flex justify-center flex-row flex-wrap gap-5'>
         {habitats.map((habitat) => (
           <Link
-            to='#'
             key={habitat.name}
+            to='#'
             style={{ backgroundImage: `url(${habitat.img})` }}
-            className='group md:w-[224.273px] md:h-[224.273px] md:rounded-full shrink-0 flex text-center justify-center items-center text-[color:var(--White,#FFF)] m-[18px]'
+            className='group h-56 w-56 rounded-full    text-white '
           >
-            <p className='opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center text-3xl not-italic font-bold leading-10'>
-              {habitat.name}
-            </p>
+            <div className='group-hover:bg-black/35 rounded-full h-full w-full flex text-center justify-center items-center'>
+              <p className='opacity-0 group-hover:opacity-100   transition-opacity duration-300 text-center text-3xl font-bold '>
+                {habitat.name}
+              </p>
+            </div>
           </Link>
         ))}
       </div>
