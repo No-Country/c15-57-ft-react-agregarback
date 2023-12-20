06fa160e9c932ae1ffbb074df69e85a35ad294b7
JRIVERADDIAZ <67973899+JRIVERADDIAZ@users.noreply.github.com>
2023-12-20 00:44:35 +0000

    Update index.jsx

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { MainLayout, PrivateLayout } from '../Layouts'
 
-import { Animals, WhoWeAre, VulnerableSpecies, Store, Register, Login, NotFound, User, MyVolunteers, MyPurchases, ResetPassword, Donations, ShippingInformation, ShoppingCart, Thanks, ShoppingSuccessful, EcosystemPage, EcosystemPageModal, AnimalsEcosystemloaderData } from '../Routes'
+import { Animals, WhoWeAre, VulnerableSpecies, Store, Register, Login, NotFound, User, MyVolunteers, MyPurchases, ResetPassword, Donations, ShippingInformation, ShoppingCart, Thanks, ShoppingSuccessful, EcosystemPage, EcosystemModal, loaderData } from '../Routes'
 
 export const router = createBrowserRouter([
   {
@@ -102,11 +102,8 @@ export const router = createBrowserRouter([
       },
        {
         path: '/Ecosystems/:id',
-        element: (
-          <Suspense fallback={<div style={{ textAlign: 'center' }}>Loading..</div>}>
-            <EcosystemPageModal />
-          </Suspense>),
-          loader: AnimalsEcosystemloaderData
+        element: <EcosystemModal />,        
+          loader: loaderData
       },
       {
         path: '/ShoppingSuccessful',
