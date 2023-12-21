17a80d525d9ed328d2b238e106bd0fd1d8413909
fabio <fabio@digitalmind.com>
2023-12-21 00:32:14 -0300

    test

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -142,7 +142,7 @@ export const router = createBrowserRouter([
         path: '/Ecosystems/',
         element: (
           <Suspense fallback={<div style={{ textAlign: 'center' }}>Loading..</div>}>
-            <InfoAnimals />
+            <EcosystemPage />
           </Suspense>
         )
       },
