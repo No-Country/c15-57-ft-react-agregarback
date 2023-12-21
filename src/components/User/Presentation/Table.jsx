a0c4f57583abcce6bd7508c92d09bb9b221b8e9b
Natsumychan <dianaczapata@gmail.com>
2023-12-21 00:37:46 -0500

    fix responsive

--- src/components/User/Presentation/Table.jsx
+++ src/components/User/Presentation/Table.jsx
@@ -1,13 +1,13 @@
 import { Fragment } from 'react'
 const Table = ({ data }) => {
   return (
-    <div className='grid grid-cols-5 text-center mb-6 '>
+    <div className='grid grid-cols-4 lg:grid-cols-5 text-center mb-6 '>
       {/* Encabezado de la tabla */}
       <div className='col-span-1 font-robotoM text-[0.8rem] sm:text-[1rem] text-gray-700 bg-gray-200 pl-2  '>ID ORDEN</div>
       <div className='col-span-1 font-robotoM text-[0.8rem] sm:text-[1rem] text-gray-700 bg-gray-200 '>FECHA</div>
       <div className='col-span-1 font-robotoM text-[0.8rem] sm:text-[1rem] text-gray-700 bg-gray-200 '>TOTAL</div>
       <div className='col-span-1 font-robotoM text-[0.8rem] sm:text-[1rem] text-gray-700 bg-gray-200 '>ESTADO</div>
-      <div className='col-span-1 bg-gray-200 text-transparent '>Detalles </div>
+      <div className='col-span-1 bg-gray-200 text-transparent hidden lg:block'>Detalles </div>
 
       {/* Datos de la tabla */}
       {data.map((data, index) => {
@@ -21,7 +21,7 @@ const Table = ({ data }) => {
               <span className='font-roboto'>{data.products}</span>
             </div>
             <div className='col-span-1 font-roboto text-[0.7rem] sm:text-[1rem] text-gray-800 mt-6'>{data.state}</div>
-            <div className='col-span-1 font-robotoM text-[0.7rem] sm:text-[1rem] text-green-600 mt-6'>Ver detalles</div>
+            <div className='hidden lg:block col-span-1 font-robotoM text-[0.7rem] sm:text-[1rem] text-green-600 mt-6'>Ver detalles</div>
           </Fragment>
         )
       }
