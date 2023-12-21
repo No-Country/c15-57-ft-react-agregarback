35d79e4c08d5e1bcde00e936cb7723f34b70a68b
fabio <fabio@digitalmind.com>
2023-12-21 16:00:19 -0300

    [Fix] NavBar sizes

--- src/components/MainNavBar/NavBar/Presentation/NavBarLinks.jsx
+++ src/components/MainNavBar/NavBar/Presentation/NavBarLinks.jsx
@@ -20,7 +20,7 @@ const NavBarLinks = ({ links }) => {
                   <li
                     key={to} className={`
                     md:hover:bg-green-900 md:p-2 md:rounded-xl text-center
-                         font-robotoM md:text-[21.459px] color-colorFooter md:hover:text-white
+                         font-robotoM text-[4vw] lg:h-[35%] md:text-[1.5vw] lg:text-[1.6vw]  2xl:text-[2.3vh] color-colorFooter md:hover:text-white
                    ${isOpen && 'w-[100%] align-middle text-[2vh] sm:text-[4vh] h-[20%]'}
                    ${!animalRouterChecker || isOpen && 'text-black w-[100%] text-[4v|h] h-[20%]'}
                    ${animalRouterChecker || isOpen && 'text-white'}
