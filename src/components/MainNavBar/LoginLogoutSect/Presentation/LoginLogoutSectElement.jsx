f87be2d5d5101c0141ebd673c43920ce65121b74
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2023-12-21 14:46:51 -0300

    [ADD] Links en Footer [FIX] Color Navbar

--- src/components/MainNavBar/LoginLogoutSect/Presentation/LoginLogoutSectElement.jsx
+++ src/components/MainNavBar/LoginLogoutSect/Presentation/LoginLogoutSectElement.jsx
@@ -11,7 +11,7 @@ export const LoginLogoutSectElement = ({ routes }) => {
                 routes.map((route) => (
                   <li
                     key={route.name} className={` flex lg:items-center p-3 rounded-xl md:hover:bg-green-900 text-center
-                         font-robotoM
+                         font-robotoM md:hover:text-white
                          text-[4vw] lg:h-[35%] md:text-[1.5vw] lg:text-[1.6vw]  2xl:text-[2.3vh]
                 ${isOpen && 'w-[100%] text-[4.1vh] h-[16.6%] justify-center'}
                 ${animalRouterChecker || isOpen ? 'text-white' : 'text-black'}
