098b7a7568474fe12be07eabd252177d399f70e3
fabio <fabio@digitalmind.com>
2023-12-22 07:53:04 -0300

    [FIX] Cantidad carrito del Navbar

--- src/components/MainNavBar/CartCounter/Presentation/CartCounterElement.jsx
+++ src/components/MainNavBar/CartCounter/Presentation/CartCounterElement.jsx
@@ -1,9 +1,9 @@
 import useRouterChecker from '../../../../Hooks/useRouterChecker'
-import { useContentContext } from '../../../../Store/contextStore/ContentContext'
+import { useECommerceContext } from '../../../../Store/contextStore/ECommerceContext'
 
 const CartCounterElement = () => {
   const { animalRouterChecker } = useRouterChecker()
-  const { totalCounting } = useContentContext()
+  const { cantidad } = useECommerceContext()
   return (
     <div className={`
   ${animalRouterChecker
@@ -13,7 +13,7 @@ const CartCounterElement = () => {
   text-center font-robotoM text-[1vh] relative md:text-[1.5vh] md:bottom-[.1vh] lg:text-[2vh] lg:bottom-[.3vh] 2xl:text-[1.5vh] 2xl:bottom-[0vh]
   `}
     >
-      {totalCounting}
+      {cantidad}
     </div>
   )
 }
